24d91b805d905f41ca6f411fb22c5ef4e74fd608
Swapnil Deshmukh <swapnil.s.deshmukh@oracle.com>
2018-01-17 19:51:03 +0530

    Hello

--- Java/Hello.java
+++ Java/Hello.java
@@ -3,7 +3,7 @@ public class Hello{
 	public static void main(String [] args){
 	
 	for (int i=1;i<=5;i++){
-		System.out.println("Hello ..."+i);
+		System.out.println("Hello ..."+i+" How are you ");
 	}
 	
 	}
